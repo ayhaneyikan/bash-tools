a225b3de7816f2dace6b32d7954ee1d38e43c95c
Ayhan Eyikan <78119087+ayhaneyikan@users.noreply.github.com>
2025-02-05 11:22:35 -0500

    Create aliases.sh

---
+++ aliases.sh
@@ -0,0 +1,28 @@
+# general
+alias ..='cd ..'
+alias b='cd -'
+
+# podman aliases
+alias pi='podman images --sort repository'
+alias pp='podman ps -a'
+
+# ls aliases
+alias l='ls --color=auto -CF1'
+alias ls='ls --color=auto'
+alias ls='ls --color=auto -CF'
+alias la='ls --color=auto -aCF'
+alias ll='ls --color=auto -lhF'
+alias lla='ls --color=auto -alhF'
+
+# git aliases
+alias gl='git log'
+alias gs='git status'
+alias gd='git diff'
+alias gb='git branch'
+alias ga='git add'
+alias gc='git commit'
+alias gp='git push'
+alias grs='git restore --staged'
+alias gsl='git stash list'
+alias gsp='git stash push'
+alias gsa='git stash apply'
