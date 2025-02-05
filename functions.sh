8c41f5539251043fe17b1bdcd100e414bcc61a4f
Ayhan Eyikan <78119087+ayhaneyikan@users.noreply.github.com>
2025-02-05 11:35:36 -0500

    Create functions.sh

---
+++ functions.sh
@@ -0,0 +1,29 @@
+##########################################
+# helper functions
+##########################################
+
+# Checks if provided arguments contains a common help argument string
+function _check_help() {
+  for arg in "$@"; do
+    if [[ "$arg" == "-h" || "$arg" == "--help" || "$arg" == "-help" || "$arg" == "--h" ]]; then
+      return 0  # indicate that help was requested
+    fi
+  done
+  return 1  # indicate that help was not requested
+}
+
+
+
+##########################################
+# personal additions
+##########################################
+
+# make directory and then cd into it
+function mkcd() {
+  local dirname=$1
+  if [ -z "$dirname" ]; then
+    echo "mkcd: missing name of directory to create and enter"
+    return
+  fi
+  mkdir $dirname && cd $dirname
+}
